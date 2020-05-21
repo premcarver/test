4a10bfdb389baca4f8269c53453005709662b74a
premchand kumar <premcarver@gmail.com>
2020-05-21 18:55:56 +0530

    Updates

---
+++ precache-manifest.6accf3756322f79f93c23bd07a160588.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21639dede53cfc2d62bbb4df59193805",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a27f7758612624515ab4",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef52c499eb8e01b649ed",
+    "url": "/test/static/js/2.19b9a9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.19b9a9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27f7758612624515ab4",
+    "url": "/test/static/js/main.6245d3d9.chunk.js"
+  },
+  {
+    "revision": "095b774a634ca7eb3132",
+    "url": "/test/static/js/runtime-main.b506e681.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
